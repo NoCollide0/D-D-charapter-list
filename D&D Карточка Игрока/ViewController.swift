fc390de86895faaa118317a572035190ee299b86
2132112313 <2131>
2023-06-01 01:58:09 +0300

    Initial Commit

---
+++ D&D Карточка Игрока/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  D&D Карточка Игрока
+//
+//  Created by Федор Шашков on 01.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
